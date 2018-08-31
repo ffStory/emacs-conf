6f636856d3d028941b4177a1e463ba55779d6567
wangff <wangff@fingerdance.cn>
2018-08-31 17:32:55 +0800

    Add ag path

--- extra.el
+++ extra.el
@@ -16,6 +16,14 @@
 
 (fset 'yes-or-no-p 'y-or-n-p)
 
+;;Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+(defun set-exec-path-from-shell-PATH ()
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(set-exec-path-from-shell-PATH)
 
 (provide 'extra)
              
